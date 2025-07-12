06f8681dc88f496cc457d9e5d37c1cf5e825ca93
hirominemoto <158188879+hirominemoto@users.noreply.github.com>
2025-07-12 13:35:43 +0900

    Update app.py
    カスタマイズ

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
 import os
 
 app = Flask(__name__)
 
+# 環境変数から読み込む場合
 line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
 
@@ -13,22 +15,18 @@ handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # オリジナルメッセージを返す！
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-if __name__ == "__main__":
-    app.run()
+        TextSendMessage(text="こんにちは！ひろみBotです！"))
 
 
 
